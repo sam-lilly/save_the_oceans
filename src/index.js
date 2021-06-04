24ac53436ffc90fde6f1807fdbb92877df23d891
Samantha Lilly <samanthalillydesign@gmail.com>
2021-06-04 02:13:43 -0400

    Add air bubbles and movement

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import Bottle from './scripts/glass_bottle';
 import Bag from './scripts/plastic_bag';
 import Cup from './scripts/solo_cup';
 import Straw from './scripts/straw';
+import Bubble from './scripts/bubble';
 
 import "./styles/index.scss";
 
@@ -28,29 +29,42 @@ const scuba = {
 
 
 const keys = [];
+const allFish = [];
 
 const rainbowFish = [];
 rainbowFish.push(new Rainbow());
 rainbowFish.push(new Rainbow());
 rainbowFish.push(new Rainbow());
+rainbowFish.forEach(fish => {
+  allFish.push(fish);
+})
 
 const nemos = [];
 nemos.push(new Nemo());
 nemos.push(new Nemo());
 nemos.push(new Nemo());
+nemos.forEach(nemo => {
+  allFish.push(nemo);
+})
 
 const octopis = [];
 octopis.push(new Octopus());
+allFish.push(octopis[0]);
 
 const jellyfishes = [];
 jellyfishes.push(new Jellyfish());
 jellyfishes.push(new Jellyfish());
+jellyfishes.forEach(jellyfish => {
+  allFish.push(jellyfish);
+})
 
 const seahorses = [];
 seahorses.push(new Seahorse());
+allFish.push(seahorses[0]);
 
 const turtles = [];
 turtles.push(new Turtle());
+allFish.push(turtles[0]);
 
 const bottles = [];
 bottles.push(new Bottle());
@@ -79,6 +93,44 @@ straws.push(new Straw());
 straws.push(new Straw());
 straws.push(new Straw());
 straws.push(new Straw());
+
+const bubbles = [];
+bubbles.push(new Bubble());
+bubbles.push(new Bubble());
+bubbles.push(new Bubble());
+bubbles.push(new Bubble());
+bubbles.push(new Bubble());
+bubbles.push(new Bubble());
+bubbles.push(new Bubble());
+bubbles.push(new Bubble());
+bubbles.push(new Bubble());
+bubbles.push(new Bubble());
+bubbles.push(new Bubble());
+bubbles.push(new Bubble());
+bubbles.push(new Bubble());
+
+bubbles.push(new Bubble());
+bubbles.push(new Bubble());
+bubbles.push(new Bubble());
+bubbles.push(new Bubble());
+bubbles.push(new Bubble());
+bubbles.push(new Bubble());
+bubbles.push(new Bubble());
+bubbles.push(new Bubble());
+bubbles.push(new Bubble());
+bubbles.push(new Bubble());
+bubbles.push(new Bubble());
+bubbles.push(new Bubble());
+bubbles.push(new Bubble());
+bubbles.push(new Bubble());
+bubbles.push(new Bubble());
+bubbles.push(new Bubble());
+bubbles.push(new Bubble());
+bubbles.push(new Bubble());
+bubbles.push(new Bubble());
+bubbles.push(new Bubble());
+bubbles.push(new Bubble());
+
 
 const scubaSprite = new Image();
 scubaSprite.src = "../src/images/scuba.png";
@@ -170,8 +222,12 @@ function animate() {
   straws[4].draw();
   straws[4].float();
 
+  drawSprite(scubaSprite, 0, 0, scuba.width, scuba.height, scuba.x, scuba.y, scuba.width, scuba.height); 
 
-  drawSprite(scubaSprite, 0, 0, scuba.width, scuba.height, scuba.x, scuba.y, scuba.width, scuba.height);
+  bubbles.forEach(bubble => {
+    bubble.draw();
+    bubble.float();
+  })
 
   moveScuba();
 
@@ -189,51 +245,78 @@ window.addEventListener("keyup", function(e) {
   delete keys[e.key];
 });
 
+let fishCount = 0;
+let bottleCount = 0;
+let bagCount = 0;
+let cupCount = 0;
+let strawCount = 0;
+
+
 function moveScuba() {
-  if (keys['ArrowUp'] && scuba.y > -28) {
+  if (keys['ArrowUp'] && scuba.y > -60) {
     scuba.y -= scuba.speed;
   }
   if (keys['ArrowDown'] && scuba.y < 255) {
     scuba.y += scuba.speed;
   }
-  if (keys['ArrowLeft'] && scuba.x >= -167) {
+  if (keys['ArrowLeft'] && scuba.x >= -210) {
     scuba.x -= scuba.speed + 2;
   }
   if (keys['ArrowRight'] && scuba.x <= 373) {
     scuba.x += scuba.speed;
   }
-  // console.log(scuba.x);
-  // console.log(scuba.y);
-  let fishCount = 0;
-  rainbowFish.forEach(fish => {
-      // if ((fish.x - (scuba.x + 242)) < -50) {
-        // works for x coordinate
-
-      // if (((fish.x - (scuba.x + 242)) < -50) && ((fish.y - (scuba.y + 96)) < 1 )) {
-      // if ((fish.x - (scuba.x + 242)) < -50) {
-        // ^^the X THAT WORKS!!!
-
-          // (((fish.x - (scuba.x + 242)) > -50) && ((fish.x - (scuba.x + 242)) < -45))
-
-        // if ((((fish.x - (scuba.x + 242)) > -60) && ((fish.x - (scuba.x + 242)) < -40)) && (((fish.y - scuba.y) < 43) && ((fish.y - scuba.y) > 35))) {
-        // works but off by a little bit on x
-
-        if ((((fish.x - (scuba.x + 242)) > -60) && ((fish.x - (scuba.x + 242)) < -10)) && (((fish.y - scuba.y) < 48) && ((fish.y - scuba.y) > 33))) {
-        // if (((fish.y - scuba.y) < 43) && ((fish.y - scuba.y) > 35)) {
-          console.log(fish.x);
-          console.log(scuba.x); 
-          console.log(fish.y);
-          console.log(scuba.y);
-          fish.x = (Math.random() * 400) + 650;
-          fish.y = Math.floor(Math.random() * 300);
-          fish.speed = Math.random() + 2.5;
-          // fishCount += 1;
-          // console.log(fishCount);
-        // }
-      }
+  allFish.forEach(fish => {
+      if ((((fish.x - (scuba.x + 242)) > -60) && ((fish.x - (scuba.x + 242)) < -10)) && (((fish.y - scuba.y) < 48) && ((fish.y - scuba.y) > 33))) {
+        fish.x = (Math.random() * 400) + 650;
+        fish.y = Math.floor(Math.random() * 300);
+        fish.speed = Math.random() + 2.5;
+        fishCount += 1;
+        console.log(fishCount);
+        // gameover();
+    }
   });
+  bottles.forEach(bottle => {
+    if ((((bottle.x - (scuba.x + 242)) > -70) && ((bottle.x - (scuba.x + 242)) < -10)) && (((bottle.y - scuba.y) < 50) && ((bottle.y - scuba.y) > 30))) {
+      bottle.x = (Math.random() * 400) + 650;
+      bottle.y = Math.floor(Math.random() * 300);
+      bottle.speed = Math.random() + 1.5;
+      bottleCount += 1;
+      console.log(bottleCount);
+    }
+  });
+  bags.forEach(bag => {
+    if ((((bag.x - (scuba.x + 242)) > -70) && ((bag.x - (scuba.x + 242)) < -10)) && (((bag.y - scuba.y) < 60) && ((bag.y - scuba.y) > 20))) {
+      bag.x = (Math.random() * 400) + 650;
+      bag.y = Math.floor(Math.random() * 300);
+      bag.speed = Math.random() + 1.25;
+      bagCount += 1;
+      console.log(bagCount);
+    }
+  });
+  cups.forEach(cup => {
+    if ((((cup.x - (scuba.x + 242)) > -70) && ((cup.x - (scuba.x + 242)) < -10)) && (((cup.y - scuba.y) < 50) && ((cup.y - scuba.y) > 30))) {
+      cup.x = (Math.random() * 400) + 650;
+      cup.y = Math.floor(Math.random() * 300);
+      cup.speed = Math.random() + 1.25;
+      cupCount += 1;
+      console.log(cupCount);
+    }
+  });
+  straws.forEach(straw => {
+    if ((((straw.x - (scuba.x + 242)) > -70) && ((straw.x - (scuba.x + 242)) < -10)) && (((straw.y - scuba.y) < 50) && ((straw.y - scuba.y) > 30))) {
+      straw.x = (Math.random() * 400) + 650;
+      straw.y = Math.floor(Math.random() * 300);
+      straw.speed = Math.random() + 1.25;
+      strawCount += 1;
+      console.log(strawCount);
+    }
+  });
+
 }
 
+// function gameover() {
+
+// }
 
 
 // can do something like if scuba.y or scuba.x (wherever the net is)
---
+++ src/scripts/bubble.js
@@ -0,0 +1,41 @@
+const bubbleSprite = new Image();
+bubbleSprite.src = "../src/images/air_bubble.png"
+
+const ctx = canvas.getContext('2d');
+canvas.width = 650;
+canvas.height = 352;
+
+function drawSprite(img, sX, sY, sW, sH, dX, dY, dW, dH) {
+    ctx.drawImage(img, sX, sY, sW, sH, dX, dY, dW, dH);
+}
+
+class Bubble {
+    constructor() {
+        this.width = 32;
+        this.height = 32;
+        this.x = Math.random() * 3000;
+        this.y = (Math.random() * 500) - 352;
+        this.speed = Math.random() + 1;
+        this.left = true;
+    }
+
+    draw() {
+        drawSprite(bubbleSprite, 0, 0, this.width, this.height, this.x, this.y, this.width, this.height);
+    }
+
+    float() {
+        this.y -= this.speed;
+        if (this.y <= -100) {
+            this.y = 352;
+            this.x = Math.random() * 3000;
+        }
+        if (this.x <= -10) {
+            this.y = 352;
+            this.x = Math.random() * 3000;
+        }
+        this.x -= 2;
+    }
+
+}
+
+export default Bubble;
